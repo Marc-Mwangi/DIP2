c3771de7237a082c4f16b47498e834aa1928390f
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-20 18:57:10 +0300

    Updating user model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -10,6 +11,13 @@ class UserProfile(models.Model):
     profile_photo = models.ImageField(upload_to='photos/user_profile')
     profile_slug= models.CharField(max_length=200)
     bio = models.CharField(max_length=4000)
+    
+    #Required fields
+    date_joined = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default= False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
     
     def __str__(self) -> str:
         return self.user_name
