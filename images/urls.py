e8934c6cb0d92bfeadac70b16e1cd6d3e45b6ce5
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-23 21:13:24 +0300

    adding allowed hosts

--- images/urls.py
+++ images/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('images/', views.Images, name= 'images'),
-    path('feed/', views.Feed, name= 'feed')
+    path('', views.Feed, name= 'feed')
 ]
